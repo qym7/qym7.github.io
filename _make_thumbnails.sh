cb7ad12262a65b4c9c08b548e5824a388783ddba
qym7 <876444960@qq.com>
2026-06-28 20:36:28 +0200

    refined

--- _make_thumbnails.sh
+++ _make_thumbnails.sh
@@ -1,6 +1,11 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
 shopt -s nullglob
+mkdir -p tn/images
+
 for file in images/*.{jpg,png,PNG}; do
     filename=$(basename "$file")
-    [ ! -f "tn/$filename" ] && magick "$file" -thumbnail 320x320 "tn/images/$filename"
+    [ ! -f "tn/images/$filename" ] && magick "$file" -thumbnail 320x320 "tn/images/$filename"
 done
 shopt -u nullglob
